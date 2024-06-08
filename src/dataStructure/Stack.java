a90947c59546acf9762def6f679908ed6ab7650f
lucarameh <lucarameh15@gmail.com>
2024-06-08 16:15:54 -0300

    Stack - getTop, getHeight, print and push methods

--- src/dataStructure/Stack.java
+++ src/dataStructure/Stack.java
@@ -18,4 +18,54 @@ public class Stack {
         this.top = new Node(data);
         this.height = 1;
     }
+
+    public void getTop() {
+        if (this.top == null) {
+            System.err.println("Stack is empty");
+        } else {
+            System.out.println(this.top.value);
+        }
+    }
+    public void getHeight() {
+        if (this.top == null) {
+            System.err.println("Stack is empty");
+        } else {
+            System.out.println(this.height);
+        }
+    }
+
+    public void print() {
+        System.out.println("###################################");
+        Node temp = this.top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("#######################################");
+    }
+
+    public void push(String v) {
+        if (this.top == null) {
+            this.top = new Node(v);
+            this.height = 1;
+        } else {
+            Node newNode = new Node(v);
+            newNode.next = this.top;
+            this.top = newNode;
+            this.height++;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack pilha =  new Stack("elm1");
+        pilha.getTop();
+        pilha.getHeight();
+        pilha.print();
+        pilha.push("elm2");
+        pilha.getTop();
+        pilha.print();
+        pilha.push("elm3");
+        pilha.print();
+        pilha.getTop();
+    }
 }
