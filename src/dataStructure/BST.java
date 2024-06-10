7c478bae2ca8a7b6dec15e9d2043b72125972a53
lucarameh <lucarameh15@gmail.com>
2024-06-10 14:07:11 -0300

    BST - Remove Value

--- src/dataStructure/BST.java
+++ src/dataStructure/BST.java
@@ -69,24 +69,55 @@ public class BST {
         else { return contains(root.right, value); }
     }
 
-    public void remove(int value) {
-
-
-
+    private Node remove(Node root, int value) {
+        if (root == null) return null;
+        else if (root.data > value) {
+            root.left = remove(root.left, value);
+        } else if (root.data < value) {
+            root.right = remove(root.right, value);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null)
+                return root.right;
+            else if (root.right == null){
+                return root.left;
+            } else {
+                int minVal = minValue(root.right).data;
+                root.data = minVal;
+                root.right = remove(root.right, minVal);
+            }
+        }
+        return root;
     }
+
+    public void remove(int value) {
+        this.root = remove(this.root, value);
+    }
+
+
+    private Node minValue(Node currentNode) {
+        if(currentNode.left != null) { return minValue(currentNode.left); }
+        else { return currentNode; }
+    }
+
 
     public static void main(String[] args) {
         BST bst = new BST();
+        bst.insert(42);
         bst.insert(1);
         bst.insert(91);
         bst.insert(3);
-        bst.insert(42);
         bst.insert(23);
         bst.insert(55);
         bst.insert(35);
         bst.insert(5);
         bst.contains(23);
         bst.contains(0);
+        bst.inOrder();
+        bst.remove(42);
+
+        bst.inOrder();
 
     }
 
